c807c81ea6bd39bc3ca61f1203bb972ba1bd4475
User0149 <floatingprecision@gmail.com>
2025-12-02 00:59:03 +1100

    clean up code for AddItemBox.tsx

--- src/components/AddItemBox.tsx
+++ src/components/AddItemBox.tsx
@@ -4,6 +4,7 @@ import { pricePerQty } from "../utils/pricePerQty";
 import ModalBox from "./ModalBox";
 import { UpdateItemContext } from "../context/UpdateItemContext";
 import { ItemsContext } from "../context/ItemsContext";
+import SpaceY from "./SpaceY";
 
 const submitAddItemForm = (e: React.FormEvent<HTMLFormElement>, setInvalidItem: setState<boolean>, items: Array<itemType>, setItemsAndUpdateLocalStorage: (itemsArray: Array<itemType>) => void, hideModals: () => void) => {    
     e.preventDefault();
@@ -75,63 +76,74 @@ export default function AddItemBox() {
 
     return (
         <ModalBox>
-            <h2 className="text-center font-bold text-2xl">Add an item</h2>
-            <form onSubmit={(e) => submitAddItemForm(e, setInvalidItem, items, setItemsAndUpdateLocalStorage, hideModals)}>
-                <div className="mb-1 text-lg font-bold">
-                    <label htmlFor="add-item-name">Item name:&nbsp;</label>
-                    <input id="add-item-name" type="text" name="item-name" className="border rounded" required></input>
-                </div>
+            <SpaceY spacing={4}>
+                <h2 className="text-center font-bold text-2xl">Add an item</h2>
+                <form onSubmit={(e) => submitAddItemForm(e, setInvalidItem, items, setItemsAndUpdateLocalStorage, hideModals)}>
+                    <SpaceY spacing={6}>
+                        {/* Name */}
+                        <div>
+                            <div className="text-lg font-bold">
+                                <label htmlFor="add-item-name">Item name:<span className="text-[red]">*</span>&nbsp;</label>
+                                <input id="add-item-name" type="text" name="item-name" className="border rounded" required></input>
+                            </div>
+                            {
+                                invalidItem && (<div className="text-[red] text-sm">
+                                    There is an item with the same name.
+                                </div>)
+                            }
+                        </div>
+                        
+                        {/* Item Properties */}
+                        <div>
+                            <div>
+                                <label htmlFor="add-selected">Selected&nbsp;</label>
+                                <input id="add-selected" type="checkbox" name="selected"></input>
+                            </div>
+                            <div>
+                                <label htmlFor="add-unit">Base unit:&nbsp;</label>
+                                <input id="add-unit" type="text" name="unit" className="border rounded w-10" value={unit} onInput={() => {setUnit((document.getElementById("add-unit") as HTMLInputElement).value)}}></input>
+                            </div>
+                        </div>
 
-                {
-                    invalidItem && (<div className="text-[red] text-sm">
-                        There is an item with the same name.
-                    </div>)
-                }
-
-                <div className="mb-1">
-                    <label htmlFor="add-selected">Selected&nbsp;</label>
-                    <input id="add-selected" type="checkbox" name="selected"></input>
-                </div>
-                <div className="mb-1">
-                    <label htmlFor="add-unit">Base unit:&nbsp;</label>
-                    <input id="add-unit" type="text" name="unit" className="border rounded w-10" value={unit} onInput={() => {setUnit((document.getElementById("add-unit") as HTMLInputElement)?.value)}}></input>
-                </div>
-
-                <br></br>
-
-                <div className="mb-1 text-lg">
-                    <label htmlFor="add-store">Store:&nbsp;</label>
-                    <input id="add-store" type="text" name="store" className="border rounded"></input>
-                </div>
-                <div>Price/quantity at store:&nbsp;</div>
-                <div className="mb-1">
-                    $
-                    <input id="add-store-price" type="text" name="store-price" className="border rounded w-12" value={storePrice} onInput={() => {setStorePrice((document.getElementById("add-store-price") as HTMLInputElement)?.value)}}></input>
-                    &nbsp;@&nbsp;
-                    <input id="add-store-quantity" type="text" name="store-quantity" className="border rounded w-10" value={storeQuantity} onInput={() => {setStoreQuantity((document.getElementById("add-store-quantity") as HTMLInputElement)?.value)}}></input>
-                    &nbsp;
-                    <input id="add-store-prefix" type="text" name="store-prefix" className="border rounded w-10 text-right" value={storePrefix} onInput={() => {setStorePrefix((document.getElementById("add-store-prefix") as HTMLInputElement)?.value)}}></input>
-                    {unit}
-                </div>
-
-                <br></br>
-                <div className="mb-1">
-                    <div>Value comparison</div>
-                    <div>
-                        $
-                        <span>{pricePerQty(Number(storePrice), Number(storeQuantity), storePrefix, Number(compQuantity), compPrefix)}</span>
-                        &nbsp;/&nbsp;
-                        <input id="add-comp-quantity" type="text" name="comp-quantity" className="border rounded w-10" value={compQuantity} onInput={() => {setCompQuantity((document.getElementById("add-comp-quantity") as HTMLInputElement)?.value)}}></input>
-                        &nbsp;
-                        <input id="add-comp-prefix" type="text" name="comp-prefix" className="border rounded w-10 text-right" value={compPrefix} onInput={() => {setCompPrefix((document.getElementById("add-comp-prefix") as HTMLInputElement)?.value)}}></input>
-                        {unit}
-                    </div>
-                </div>
-
-                <br></br>
-
-                <input type="submit" className="border rounded p-2 cursor-pointer bg-[#eeeeee] hover:bg-[lightgray]" value="Add item"></input>
-            </form>
+                        {/* Store */}
+                        <SpaceY spacing={2}>
+                            <div className="text-lg font-bold">
+                                <label htmlFor="add-store">Store:&nbsp;</label>
+                                <input id="add-store" type="text" name="store" className="border rounded"></input>
+                            </div>
+                            <div>
+                                <div className="font-bold">Price/quantity at store:&nbsp;</div>
+                                <div>
+                                    $
+                                    <input type="text" name="store-price" className="border rounded w-12" value={storePrice} onInput={(e) => {setStorePrice((e.target as HTMLInputElement).value)}}></input>
+                                    &nbsp;@&nbsp;
+                                    <input type="text" name="store-quantity" className="border rounded w-10" value={storeQuantity} onInput={(e) => {setStoreQuantity((e.target as HTMLInputElement).value)}}></input>
+                                    &nbsp;
+                                    <input type="text" name="store-prefix" className="border rounded w-10 text-right" value={storePrefix} onInput={(e) => {setStorePrefix((e.target as HTMLInputElement).value)}}></input>
+                                    {unit}
+                                </div>
+                            </div>
+                        </SpaceY>
+                        
+                        {/* Comparison */}
+                        <div>
+                            <div className="font-bold">Value comparison</div>
+                            <div>
+                                $
+                                <span>{pricePerQty(Number(storePrice), Number(storeQuantity), storePrefix, Number(compQuantity), compPrefix)}</span>
+                                &nbsp;/&nbsp;
+                                <input type="text" name="comp-quantity" className="border rounded w-10" value={compQuantity} onInput={(e) => {setCompQuantity((e.target as HTMLInputElement).value)}}></input>
+                                &nbsp;
+                                <input type="text" name="comp-prefix" className="border rounded w-10 text-right" value={compPrefix} onInput={(e) => {setCompPrefix((e.target as HTMLInputElement).value)}}></input>
+                                {unit}
+                            </div>
+                        </div>
+                        
+                        {/* Submit */}
+                        <input type="submit" className="border rounded p-2 cursor-pointer bg-[#eeeeee] hover:bg-[lightgray]" value="Add item"></input>
+                    </SpaceY>
+                </form>
+            </SpaceY>
         </ModalBox>
     );
 }
