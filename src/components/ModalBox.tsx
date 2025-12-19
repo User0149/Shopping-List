b4da7e4123212474f96762ab10fa3e37d1b999b5
User0149 <floatingprecision@gmail.com>
2025-12-19 19:31:49 +1100

    added bottom padding for modal box

--- src/components/ModalBox.tsx
+++ src/components/ModalBox.tsx
@@ -10,8 +10,8 @@ export default function ModalBox({ children }: ModalBoxProps) {
     const { hideModals } = useContext(ModalsContext);
     
     return (
-        <div className="fixed left-0 top-0 h-full w-full z-1000 bg-black/75" onClick={hideModals}>
-            <div className="mx-auto mt-8 p-4 w-3/4 z-1001 bg-white" onClick={(e) => {
+        <div className="fixed left-0 top-0 h-full w-full z-1000 overflow-auto bg-black/75" onClick={hideModals}>
+            <div className="mx-auto my-8 p-4 w-3/4 z-1001 bg-white" onClick={(e) => {
                 e.stopPropagation();
             }}>
                 <span className="float-right cursor-pointer text-4xl text-[lightgray] hover:text-[gray]" onClick={hideModals}>&times;</span>
